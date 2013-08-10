da327bc4b99ddf3c31263ae66e24c51e8b9f3279
Ubuntu <ubuntu@ip-10-253-23-239.us-west-2.compute.internal>
2013-08-10 18:31:46 +0000

    Implemented name selection logic, resume from file

--- get_baby_names.rb
+++ get_baby_names.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/ruby
 
 require 'json'
-require 'pp'
 
 # Get bulk list of names, separate by gender
 # Remove names of facebook friends
@@ -26,23 +25,20 @@ def review_names(names)
   }
 
   choice = ""
-  while true
-    puts "\nPlease enter the number of the names you're interested in."
+  while choice != "x"
+    puts "Please enter the number of the names you're interested in."
     name_subset = names.shift(10)
     name_subset.each_with_index { |name, i| puts "[#{i}] #{name}" }
     puts "[x] Exit to main menu"
     choice = $stdin.gets.downcase.strip
     break if choice == 'x'
-    next if choice == ""
-    choices = choice.scan(/\d/).map(&:to_i).uniq
-    chosen_names = choices.map { |i| name_subset[i] }
-    puts "Adding #{chosen_names.join(", ")}" 
-    reviewed_names[:selected] += chosen_names
-	end
+    choices = choice.scan(/\d/)
+
+    reviewed_names[:selected] << name_subset.each_with_index.select { |name, i| choices.include?(i) }
+  end
 
   reviewed_names[:to_review] = names
 
-  puts "You've selected the following names: #{reviewed_names[:selected].join(", ")}, with #{reviewed_names[:to_review].size} names left to review."
   reviewed_names
 end
 
@@ -50,7 +46,6 @@ def present_main_menu(names)
   choice = ""
 
   while choice != "q"
-    puts "Please make a selection from the menu below and press Enter."
     puts %q{
     [b] - Choose boy names
     [g] - Choose girl names
@@ -61,44 +56,27 @@ def present_main_menu(names)
     case choice
     when "b"
       reviewed_names = review_names(names[:to_review][:boy])
-      names[:selected][:boy] = reviewed_names[:selected] | names[:selected][:boy]
+      puts "Reviewed names"
+      puts reviewed_names[:selected]
+      puts reviewed_names[:to_review].size
+      puts reviewed_names.size
+      names[:selected][:boy] = reviewed_names[:selected] & names[:selected][:boy]
       names[:to_review][:boy] = reviewed_names[:to_review]
-      puts "Complete list of selected names: #{names[:selected][:boy].join(", ")}"
     when "g"
-      reviewed_names = review_names(names[:to_review][:girl])
-      names[:selected][:girl] = reviewed_names[:selected] | names[:selected][:girl]
-      names[:to_review][:girl] = reviewed_names[:to_review]
-      puts "Complete list of selected names: #{names[:selected][:girl].join(", ")}"
+      
     when "r"
-      filename = "./baby_names.resume"
-      puts "Please enter the name of the file you'd like to resume from. (default #{filename.inspect})"
-      entered_filename = $stdin.gets.strip
-      filename =  entered_filename unless entered_filename == ""
-      if File.exists? filename 
-        begin
-         print "Reading in #{filename}..."
-         json = File.read(filename)
-         names = JSON.parse(json, {:symbolize_names => true})
-         puts "Success!"
-         puts "Overall selected names: #{names[:selected][:boy].join(", ")}"
-         puts "Names to review:"
-         puts "\t Boy: #{names[:to_review][:boy].size}" 
-         puts "\t Girl: #{names[:to_review][:girl].size}" 
-        rescue => e
-          puts "Error reading file: #{e}"
-        end
-      else
-        puts "The file doesn't exist."
-      end
+      names = JSON.parse(File.read("./baby_names.resume"))
     when "q"
-	  File.open("./baby_names.resume", "w") { |f| f.write names.to_json }
       break
     else
       puts "Choice not recognized, please try again."
     end
   end
 
+  File.open("./baby_names.resume", "w") { |f| f.write names.to_json }
 end
+
+
 
 puts "Welcome to the Baby Name Chooser 3000!"
 
@@ -118,11 +96,12 @@ names[:to_review].keys.each do |gender|
   puts "\tfor #{gender.to_s}s..."
   Dir.glob("./#{gender.to_s}/*.names").each do |file|
     names_from_file = get_names(file)
-    names[:to_review][gender] = (names[:to_review][gender] | names_from_file).sort
+    names[:to_review][gender] = names[:to_review][gender] | names_from_file
   end
 end
 
 names = names.merge(remove_ambiguous_names(names))
 names[:to_review].each { |gender, names| puts "Got #{names.size} #{gender.to_s} names." }
+
 
 present_main_menu(names)
